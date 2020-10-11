0fffd731b38425e631ef6a93ffe8e549be377534
Cerberuuus <m.balogh999@gmail.com>
2020-10-11 22:23:52 +0200

    Fix of memory leak check

--- run_memory_leak_check.sh
+++ run_memory_leak_check.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 IFS=$'\n'
-run_program="$(./a.out units/unit1.json units/unit2.json)"
-memory_leak_check_cmd="$(valgrind --leak-check=yes --log-file=memory_leak_check.txt $run_program)"
-echo $memory_leak_check_cmd
+memory_leak_check_cmd="$(valgrind --leak-check=yes --log-file=memory_leak_check.txt ./a.out units/unit1.json units/unit2.json)"
 memory_leak_check_result="$(cat ./memory_leak_check.txt)"
-detected_errors="$(echo $memory_check_result | sed 's/^.*ERROR SUMMARY: \([0-9]*\) errors.*$/\1/')"
+echo $memory_leak_check_result
+detected_errors="$(echo $memory_leak_check_result | sed 's/^.*ERROR SUMMARY: \([0-9]*\) errors.*$/\1/')"
 expected_errors="0"
 if [ "$expected_errors" == "$detected_errors" ]
 then
+    echo
     echo "No memory leak(s) found."
 else
     echo "Memory leak(s) found. Quitting."
