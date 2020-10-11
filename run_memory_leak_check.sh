076f68642382318cea6559812dcd1aeceea11fe5
Cerberuuus <m.balogh999@gmail.com>
2020-10-11 22:10:01 +0200

    Fix and refactoring

--- run_memory_leak_check.sh
+++ run_memory_leak_check.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 IFS=$'\n'
-run_all="$(./run_all.sh)"
-memory_check_result="$(cat ./memory_leak_check.txt)"
+run_program="$(./a.out units/unit1.json units/unit2.json)"
+memory_leak_check_cmd="$(valgrind --leak-check=yes --log-file=memory_leak_check.txt $run_program)"
+echo $memory_leak_check_cmd
+memory_leak_check_result="$(cat ./memory_leak_check.txt)"
 detected_errors="$(echo $memory_check_result | sed 's/^.*ERROR SUMMARY: \([0-9]*\) errors.*$/\1/')"
 expected_errors="0"
 if [ "$expected_errors" == "$detected_errors" ]
